c69a502b77cf326eb91c903f143f9d4bce3ea115
lukyanov <roman.lukyanov@geotoolkit.net>
2023-01-22 21:37:48 +0400

    Added example of table with only horizontal spanes

--- src/tree.ts
+++ src/tree.ts
@@ -14,51 +14,131 @@ export const simpleTable: TreeTable = {
     Name: "SimpleTable",
     Children: [{
         VerticalSpan: 1,
-        Color: 'yellow',
+        Color: 'teal',
         Value: '1',
         Children: [{
             VerticalSpan: 1,
-            Color: 'yellow',
+            Color: 'teal',
             Value: '4',
             Children: [{
                 VerticalSpan: 1,
-                Color: 'yellow',
+                Color: 'teal',
                 Value: '7',
                 Children: []
             }]
         }]
     }, {
         VerticalSpan: 1,
-        Color: 'yellow',
+        Color: 'teal',
         Value: '2',
         Children: [{
             VerticalSpan: 1,
-            Color: 'yellow',
+            Color: 'teal',
             Value: '5',
             Children: [{
                 VerticalSpan: 1,
-                Color: 'yellow',
+                Color: 'teal',
                 Value: '8',
                 Children: []
             }]
         }]
     }, {
         VerticalSpan: 1,
-        Color: 'yellow',
+        Color: 'teal',
         Value: '3',
         Children: [{
             VerticalSpan: 1,
-            Color: 'yellow',
+            Color: 'teal',
             Value: '6',
             Children: [{
                 VerticalSpan: 1,
-                Color: 'yellow',
+                Color: 'teal',
                 Value: '9',
                 Children: []
             }]
         }]
     }]
 };
+
+export const onlyHorizontalSpansTable: TreeTable = {
+    Name: "SimpleTable",
+    Children: [{
+        Color: 'teal',
+        Value: '1',
+        VerticalSpan: 1,
+        Children: [{
+            Color: 'teal',
+            Value: '4',
+            VerticalSpan: 1,
+            Children: [{
+                Color: 'teal',
+                Value: '10',
+                VerticalSpan: 1,
+                Children: []
+            }]
+        }, {
+            Color: 'teal',
+            Value: '5',
+            VerticalSpan: 1,
+            Children: [{
+                Color: 'teal',
+                Value: '11',
+                VerticalSpan: 1,
+                Children: []
+            }]
+        }, {
+            Color: 'teal',
+            Value: '6',
+            VerticalSpan: 1,
+            Children: [{
+                Color: 'teal',
+                Value: '12',
+                VerticalSpan: 1,
+                Children: []
+            }]
+        }]
+    }, {
+        Color: 'teal',
+        Value: '2',
+        VerticalSpan: 1,
+        Children: [{
+            Color: 'teal',
+            Value: '7',
+            VerticalSpan: 1,
+            Children: [{
+                Color: 'teal',
+                Value: '13',
+                VerticalSpan: 1,
+                Children: []
+            }]
+        }]
+    }, {
+        Color: 'teal',
+        Value: '3',
+        VerticalSpan: 1,
+        Children: [{
+            Color: 'teal',
+            Value: '8',
+            VerticalSpan: 1,
+            Children: [{
+                Color: 'teal',
+                Value: '14',
+                VerticalSpan: 1,
+                Children: []
+            }]
+        }, {
+            Color: 'teal',
+            Value: '9',
+            VerticalSpan: 1,
+            Children: [{
+                Color: 'teal',
+                Value: '15',
+                VerticalSpan: 1,
+                Children: []
+            }]
+        }]
+    }]
+}
 
 export const defaultTreeTable: TreeTable = {
     Name: "Table",
